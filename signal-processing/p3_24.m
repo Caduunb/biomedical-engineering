16ca84d6aa71f4c4460f6f4262508875404ab2b7
Caio Eduardo Coelho de Oliveira <ceco.unb@gmail.com>
2019-06-19 18:50:24 -0300

    3.23 and 3.24 plots

--- signal-processing/p3_24.m
+++ signal-processing/p3_24.m
@@ -4,19 +4,31 @@ clearvars; close all; clc
 load ('p2_36data.mat')
 
 fs = 200;                   % sampling frequency 
-N = 512;                    % points
+N  = 512;                    % points
 freq_vec = 0:(fs/N):fs/2;   % frequency vector
 %% Power Spectrum
-[PS_avg1,f1] = pwelch(gauss_noise,32);
-[PS_avg2,f2] = pwelch(gauss_noise,256);
+[gaussPS_avg32,freq_vector32]  = pwelch(gauss_noise,32);
+[gaussPS_avg256,freq_vector256] = pwelch(gauss_noise,256);
+[filteredPS_avg32,freq_vector32]  = pwelch(filter_noise,32);
+[filteredPS_avg256,freq_vector256] = pwelch(filter_noise,256);
 
 %% Plot
-figure(1);
-plot(f1,PS_avg1,'b', 'LineWidth', 2);
-hold on;
-plot(f2,PS_avg2,'m', 'LineWidth', 2);
+% Raw gaussian data
+figure(1); hold on;
+sgtitle('Raw Gaussian Noise');
+subplot(2,1,1);
+plot(freq_vector32, gaussPS_avg32,'b', 'LineWidth', 2);
+legend('32-point Welch Transform', 'FontSize', 12);
+subplot(2,1,2);
+plot(freq_vector256, gaussPS_avg256,'m', 'LineWidth', 2);
+legend({'256-point Welch Transform'}, 'FontSize', 12)
 
-
-
-
-
+% Filtered Gaussian Noise
+figure(2); hold on;
+sgtitle('Filtered Gaussian Noise');
+subplot(2,1,1);
+plot(freq_vector32,filteredPS_avg32,'b', 'LineWidth', 2);
+legend('32-point Welch Transform', 'FontSize', 12)
+subplot(2,1,2);
+plot(freq_vector256,filteredPS_avg256,'m', 'LineWidth', 2);
+legend('256-point Welch Transform', 'FontSize', 12)
